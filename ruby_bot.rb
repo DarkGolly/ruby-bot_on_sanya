29c2ef0c22cc46ec0e60f2c798dc107e9243f741
DarkGolly <67584569+DarkGolly@users.noreply.github.com>
2021-02-01 18:26:24 +0300

    Delete ruby_bot.rb
    del garbage

--- ruby_bot.rb
+++
@@ -1,9 +0,0 @@
-require 'discordrb'
-
-bot = Discordrb::Bot.new token: 'ODA1MzY0NDkyNzkxODQwNzk4.YBZ0Ig.dx3_A857H4Z9px1p0DlxEpXBAxs'
-
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
-end
-
-bot.run
